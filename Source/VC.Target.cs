f4eded484da2d09574b0f01c657eaabd467330eb
EPICGameGuy <admin@cactus.vg>
2024-06-06 14:53:56 -0400

    Init commit

---
+++ Source/VC.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class VCTarget : TargetRules
+{
+	public VCTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("VC");
+	}
+}
