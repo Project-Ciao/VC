3809dec74b96707f849c7b933c8ffbe603ddfa10
EPICGameGuy <admin@cactus.vg>
2024-07-20 11:43:57 -0700

    Added build scripts

--- Build/push_windows_beta.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir butler
-rm -rfv butler/VC
-cp -r Windows/ butler/VC
-rm -rfv ./butler/VC/VC/Saved
-rm -rfv ./butler/VC/FileOpenOrder
-rm -rfv ./butler/VC/Manifest*.txt
-./butler.exe push --ignore '*.pdb' butler/ epicgameguy/vc:windows
-read -p "Press any key to close window..."
